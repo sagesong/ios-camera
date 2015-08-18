0af64410b04645103a787433598c9b351c57f674
lightning <songjianw@gmail.com>
2015-08-18 14:23:22 +0800

    Initial Commit

---
+++ PDSCameraModule/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PDSCameraModule
+//
+//  Created by Lightning on 15/8/18.
+//  Copyright (c) 2015年 Lightning. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PDSCameraModule/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PDSCameraModule
+//
+//  Created by Lightning on 15/8/18.
+//  Copyright (c) 2015年 Lightning. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
